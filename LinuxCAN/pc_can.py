b773f67882fecc4fe22b4094fddafd101dcea724
rudals2710 <82440673+rudals2710@users.noreply.github.com>
2023-04-08 15:56:42 +0900

    Update pc_can.py

--- LinuxCAN/pc_can.py
+++ LinuxCAN/pc_can.py
@@ -1,11 +1,14 @@
 import can
 
 class Can:
-    def __init__(self, dev='can0'):
+    def __init__(self, dev='can0'): # can.interface.Bus 객체 생성
         self.bus=can.interface.Bus(channel=dev,bustype='socketcan',bitrate=500000)
 
-    def filter(self,id,mask=0x7FF):
+    def __del__(self):
         self.bus.shutdown()
+        
+    def filter(self,id,mask=0x7FF):
+        self.bus.set_filters([{'can_id':id,"can_mask":mask}])
 
     def send(self,id,payload):
         msg=can.Message(arbitration_id=id,data=payload,is_extended_id=False)
